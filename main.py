cc572a17883a7769eefeee31dcf399f2d7bb5678
Imrane Lawani <naimlawani01@gmail.com>
2024-09-22 22:27:52 +0200

    fix: CORSMiddleware

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # app/main.py
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from config import init_db
 from routes import user, teacher, student, post_like, post, message
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PATCH","PUT", "DELETE"],
+    allow_headers=["*"],
+)
 
 # Include routers
 app.include_router(user.router, prefix="/users", tags=["Users"])
